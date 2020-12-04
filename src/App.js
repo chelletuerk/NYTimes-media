82d48094ccad585ea7e75d4c2f4632bca81944b9
chelle tuerk <tuerkbot@chelles-MacBook-Pro-2.local>
2020-12-04 14:49:07 -0700

    hitting NYTimes api and rendering abstract text

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 import React, { Component } from 'react';
 
 class App extends Component {
-
-    render() {
-      return (
-      <div></div>
-      )
+  constructor() {
+    super()
+    this.state = {
+      article: [],
     }
   }
+
+  componentDidMount() {
+    fetch(`https://api.nytimes.com/svc/news/v3/content/all/all.json?api-key=7Q3tvUFMHDhcZIFn4MyNAc0FBos0WzSE`)
+    .then((res) => res.json())
+    .then(data => this.setState({article: data.results.map((e, i) => {
+      return(
+        <div className='article' key={i}>
+          {e.abstract}
+        </div>
+      )
+    })
+  }))
+}
+
+  render() {
+    return (
+      <div className='App'>
+        <div id='articleList' className="list">{this.state.article}</div>
+      </div>
+    )
+  }
+}
 
 export default App;
