e22ca126ceafa3cf2b83f07266d9772f66e15f68
chelle tuerk <tuerkbot@chelles-MacBook-Pro-2.local>
2020-12-04 16:14:24 -0700

    sliced 1st article out of state, formatted date, added abstract pop-up on hover

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import Button from './Button'
-import './App.css'
-
+import './App.css';
 
 class App extends Component {
   constructor() {
     super()
     this.state = {
       article: [],
+      footerDate: '',
+      firstArticleImg: [],
+      firstArticleTitle: '',
+      firstArticleAbstract: [],
+      firstArticleDate: [],
+      firstArticleUrl: '',
     }
   }
 
@@ -15,21 +20,52 @@ class App extends Component {
     fetch(`https://api.nytimes.com/svc/news/v3/content/all/all.json?api-key=7Q3tvUFMHDhcZIFn4MyNAc0FBos0WzSE`)
     .then((res) => res.json())
     .then(data => this.setState({article: data.results.map((e, i) => {
-      return(
-        <div className='article' key={i}>
-          <img className='thumbnail' alt='' src={e.thumbnail_standard}>
-          </img>< br/>
-          <span>
-            <a className='title' href={e.url} rel="noreferrer" target="_blank">
-            {e.title}</a>< br/>< br/>
+      const date = e.first_published_date.split('T')[0]
+      const firstArticleImg = data.results[0].thumbnail_standard
+      const firstArticleTitle = data.results[0].title
+      const firstArticleAbstract = data.results[0].abstract
+      const firstArticleDate = data.results[0].first_published_date
+        .split('T')[0] + ' at ' + data.results[0].first_published_date
+        .split('T')[1]
+        .split('-')[0]
+      const firstArticleUrl = data.results[0].url
+      this.setState({footerDate: date})
+      this.setState({firstArticleImg: firstArticleImg})
+      this.setState({firstArticleTitle: firstArticleTitle})
+      this.setState({firstArticleAbstract: firstArticleAbstract})
+      this.setState({firstArticleDate: firstArticleDate})
+      this.setState({firstArticleUrl: firstArticleUrl})
+
+      if (data.results.multimedia !== undefined) {
+        return(
+          <div id ='articleList' className='article' key={i}>
+            <img className='thumbnail' alt='' src={e.multimedia[1].url}></img>< br/>
+            <span><a className='title' href={e.url} rel="noreferrer" target="_blank">{e.title}</a>< br/>< br/>
             {e.abstract}< br/>< br/>
-            Published: {e.first_published_date.split('T')[0]}
-            at {e.first_published_date.split('T')[1]}
-          </span>
-        </div>
-      )
-    })
-  }))
+            Published: {e.first_published_date.split('T')[0]} at {e.first_published_date.split('T')[1].split('-')[0]}</span>
+          </div>
+          )
+      } else {
+        return(
+          <div className='article' key={i}>
+            <img
+              className='thumbnail'
+              alt=''
+              src={e.thumbnail_standard}>
+            </img>< br/>
+            <span>
+              <a
+                className='title'
+                href={e.url} rel="noreferrer"
+                target="_blank">{e.title}
+              </a>< br/>< br/>
+              {e.abstract}< br/>< br/>
+              Published: {e.first_published_date.split('T')[0]} at {e.first_published_date.split('T')[1].split('-')[0]}
+            </span>
+          </div>
+        )
+      }
+  })}))
 }
 
   render() {
@@ -38,10 +74,25 @@ class App extends Component {
         <div className='banner'>
           <span className='nytLogo'></span>
         </div>
-        <div id='articleList' className="list">{this.state.article}</div>
+        <div className='firstArticle'>
+          <img className='firstThumbnail' alt=''
+          src={this.state.firstArticleImg}></img>< br/>
+          <span className='firstArticleSpan'>
+            <a className='title' href={this.state.firstArticleUrl}
+            rel='noreferrer' target='_blank'>{this.state.firstArticleTitle}
+            </a>< br/>< br/>
+            {this.state.firstArticleAbstract}< br/>< br/>
+            {this.state.firstArticleDate}
+          </span>
+        </div>
+        <div id='articleList' className="list">
+          {this.state.article.slice(1)}
+        </div>
+        <div className='footer'>
+        <div className='date'>{this.state.footerDate}</div>
         <Button
           className='subscribe'
-          text='Subscribe'/>
+          text='Subscribe' />
         <Button
           className='digitalEdition'
           text='Digital Edition' />
@@ -51,6 +102,7 @@ class App extends Component {
         <Button
           className='renewal'
           text='Renewal' />
+        </div>
       </div>
     )
   }
